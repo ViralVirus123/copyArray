3c9e485e9f8ab57635ec71bc7afd7d59244bce64
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-12-01 17:41:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+  String [] roster1 = {"Jeff", "Daniel", "Manny", "Dan", "David", "Sofia", "Stella", "Bella", "Blake", "Robin"};
+    int [] roster2 = {1,2,3,4,5,6,7,8,9,10};
+  for (int i = 0; i < roster1.length; i++)
+  {
+    System.out.println(roster1[i] + roster2[i]);
+  }
+  }
+}
