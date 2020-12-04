8237202fe9ad0ffde3f8d4f028e0bb86b53b9c52
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-12-04 04:23:00 +0000

    Fixed code

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 class Main {
   public static void main(String[] args) {
-  String [] roster1 = {"Jeff", "Daniel", "Manny", "Dan", "David", "Sofia", "Stella", "Bella", "Blake", "Robin"};
-    int [] roster2 = {1,2,3,4,5,6,7,8,9,10};
-  for (int i = 0; i < roster1.length; i++)
+  String [] roster1 = new String[10];
+  String [] roster2 = {"Jeff", "Daniel", "Manny", "Dan", "David", "Sofia", "Stella", "Bella", "Blake", "Robin"};
+
+  for (int index = 0; index < roster1.length; index++)
   {
-    System.out.println(roster1[i] + roster2[i]);
+    roster1[index] = roster2[index];
+    System.out.println(roster1[index]);
   }
   }
 }
